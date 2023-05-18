da96f7a3897d9cb0d49c8bc053789ea350d3b11e
Shivam Chaudhary <shivamchaudhary310899@gmail.com>
2023-05-18 13:31:22 +0530

    create number objects

---
+++ 1.js
@@ -0,0 +1,15 @@
+function createNumberObjects(numbers) {
+    return numbers.map((number) => {
+      return {
+        number: number,
+        isEven: number % 2 === 0,
+        double: number * 2
+      };
+    });
+  }
+  
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const numberObjects = createNumberObjects(numbers);
+  console.log(numberObjects);
+
+  
